97a49f27a4f7403ef1985b610ded8f0656c0616a
txrunishere <tsoni9286@gmail.com>
2025-08-31 11:23:26 +0530

    Deploy

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 import { RegisterForm } from "@/components";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
